7e860f01435c72c55c021d49c0b1439d483abd2d
valeria.castresana <castre.vale@gmail.com>
2022-07-28 16:17:56 -0300

    agregando Fetch

--- js/nuevo.js
+++ js/nuevo.js
@@ -1,40 +1,40 @@
 
-const contenedorProductos = document.getElementById('producto-contenedor')
-
-const contenedorCarrito = document.getElementById('carrito-contenedor')
-
-const botonVaciar = document.getElementById('vaciar-carrito')
-
-const contadorCarrito = document.getElementById('contadorCarrito')
-
 const cantidad = document.getElementById('cantidad')
 const precioTotal = document.getElementById('precioTotal')
 const cantidadTotal = document.getElementById('cantidadTotal')
 
-
 let carrito = []
 
-stockProductos.forEach((producto) => {
-    const div = document.createElement('div')
+const contenedorProductos = document.getElementById('producto-contenedor');
+
+fetch("./stock.json")
+    .then( (response) => response.json() )
+    .then( (data) => {
+        mostrarProductos(data);
+    })
+
+const mostrarProductos = (data) => {
+    data.forEach(producto => {
+    const div = document.createElement("div");
     div.classList.add('card')
-    div.innerHTML =  ` <div class="productos-item">
+    div.innerHTML =  
+    ` <div class="productos-item">
     <img class="foto-remera" src=${producto.img}>
     <h3 class="modelo-remera">${producto.modelo}</h3>
     <p class="precio">$  ${producto.precio}</p>
     <p class="medio-pagos">${producto.medioPago}</p>    
     <div class="selectores">
     <select name="select" id="selectorColor" class="selectorColor">
-    <option value="value0">Color</option>
-    <option value="value1" class="color"> ${producto.color.NE}</option>
-    <option value="value2" class="color"> ${producto.color.BL}</option>
-    <option value="value3" class="color"> ${producto.color.BO}</option>
+    <option id="${producto.color.NE}" class="color"> ${producto.color.NE}</option>
+    <option id="${producto.color.BL}" class="color"> ${producto.color.BL}</option>
+    <option id="${producto.color.BO}" class="color"> ${producto.color.BO}</option>
     </select> 
     <select name="select" id="selectorTalle" class="selectorTalle">
-    <option value="value0">Talle</option>
-    <option value="value1" class="talle">${producto.talle.S}</option>
-    <option value="value2" class="talle">${producto.talle.M}</option>
-    <option value="value3" class="talle">${producto.talle.L}</option>
+    <option value="${producto.talle.S}"  class="talle">${producto.talle.S}</option>
+    <option value="${producto.talle.M}" class="talle">${producto.talle.M}</option>
+    <option value="${producto.talle.L}" class="talle">${producto.talle.L}</option>
     </select> 
+    <input type="number" name="cant" id="selectorCantidad" class="selectorCantidad"  min="1" max="5" value="1" />
     </div>
     <button id="agregar${producto.id}" class="boton-agregarCarrito" id="boton-agregarCarrito">Comprar</button>   
                     
@@ -45,6 +45,15 @@ stockProductos.forEach((producto) => {
     const boton = document.getElementById(`agregar${producto.id}`)
     boton.addEventListener('click', () => {
 
+        const colorRemera = document.querySelector(`#agregar${producto.id}`).parentElement.querySelector('#selectorColor').value;
+        const talleRemera = document.querySelector(`#agregar${producto.id}`).parentElement.querySelector('#selectorTalle').value;
+     
+        const cantRemera = document.querySelector(`#agregar${producto.id}`).parentElement.querySelector('#selectorCantidad').value;
+        console.log(cantRemera);
+
+
+        console.log(colorRemera, talleRemera, cantRemera);
+
         swal({
             title: "Salud!",
             text: "Tu producto se agregó al carrito con éxito", 
@@ -52,35 +61,51 @@ stockProductos.forEach((producto) => {
             confirm: "ok",
           })
 
-        agregarAlCarrito(producto.id)
+        agregarAlCarrito(producto.id, colorRemera, talleRemera, cantRemera)
     })
-})
-  
+});
+}
 
-function agregarAlCarrito(id){
+function agregarAlCarrito(id, colorRemera, talleRemera, cantRemera){
 
-    let item = stockProductos.find(prod => prod.id === id) 
-    let productoEnCarrito = carrito.find(item => item.id === id)
+    fetch("./stock.json")
+    .then( (response) => response.json() )
+    .then( (data) => {
+        let item = data.find(prod => prod.id === id) 
+        item.color = colorRemera;
+        item.talle = talleRemera;
+        item.cantidad = cantRemera;
+        console.log(item);
 
-    if (productoEnCarrito){ 
-        productoEnCarrito.cantidad++;
-    }else { 
-        item.cantidad = 1;
-        carrito.push(item)
-    }
-    
-    actualizarCarrito() 
+        let productoEnCarrito = carrito.find(item => item.id === id)
+
+        if (productoEnCarrito){ 
+            productoEnCarrito.cantidad++
+        }else { 
+
+            carrito.push(item)
+        }
+        
+        actualizarCarrito() 
+    })
 }
 
 
 const eliminarDelCarrito = (id) => {
+
     const item = carrito.find((prod) => prod.id === id)
 
     const indice = carrito.indexOf(item)
     carrito.splice(indice, 1) 
     actualizarCarrito() 
     console.log(carrito)
+
+
 }
+
+const contenedorCarrito = document.getElementById('carrito-contenedor')
+
+const contadorCarrito = document.getElementById('contadorCarrito')
 
 const actualizarCarrito = () => {
 
@@ -90,9 +115,12 @@ const actualizarCarrito = () => {
         const div = document.createElement('div')
         div.className = ('productoEnCarrito')
         div.innerHTML = `
-        
+
+        <img src="${prod.img}" class="imgModal">
         <div>
         <p><b>Modelo:</b> ${prod.modelo}</p>
+        <p><b>Color:</b> ${prod.color}</p>
+        <p><b>Talle:</b> ${prod.talle}</p>
         </div>
         <p><b>Precio:</b> $ ${prod.precio}</p>
         <p><b>Cantidad:</b> ${prod.cantidad}</p>
@@ -103,9 +131,7 @@ const actualizarCarrito = () => {
         
         localStorage.setItem('carrito', JSON.stringify(carrito))
 
-    })
-
-   
+    })   
 
     contadorCarrito.innerText = carrito.length 
     console.log(carrito)
@@ -120,7 +146,29 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 })
 
+const botonVaciar = document.getElementById('vaciar-carrito')
+
 botonVaciar.addEventListener('click', () => {
+
+    swal({
+        title: "Atención",
+        text: "Todos los productos se borraran del carrito ",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+          swal("El carrito ha quedado vacío", {
+            icon: "success",
+          });
+          carrito.length = 0
+          actualizarCarrito()
+        } else {
+          swal("Podes seguir con tu compra");
+        }
+      });
+
     carrito.length = 0
     actualizarCarrito()
 })
